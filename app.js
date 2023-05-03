2c3ec19e519bae389574fd3f1055cdf20debf99f
Iryna Khamidova <waterholein@gmail.com>
2023-05-03 22:28:09 +0300

    Add avatar

--- app.js
+++ app.js
@@ -3,10 +3,6 @@ const express = require('express')
 const logger = require('morgan')
 const cors = require('cors')
 require('dotenv').config()
-const multer = require('multer')
-const path = require('path')
-const fs = require('fs');
-const {nanoid} = require('nanoid')
 
 const authRouter = require('./routes/api/auth')
 const contactsRouter = require('./routes/api/contacts')
@@ -18,49 +14,18 @@ const formatsLogger = app.get('env') === 'development' ? 'dev' : 'short'
 app.use(logger(formatsLogger))
 app.use(cors())
 app.use(express.json())
-app.use(express.static('public'));
-
-const tempDir = path.join(__dirname, 'temp')
-
-const multerConfig = multer.diskStorage({
-  destination: tempDir, 
-  filename: (req, file,cb) => {
-    cb(null, file.originalname);
-  }
-})
-
-const upload = multer({
-  storage: multerConfig
-})
-
-const avatars = [];
-
-app.get('/api/avatars', (req, res) => {
-res.json(avatars);
-})
-const avatarDir = path.join(__dirname, 'public', 'avatars');
-app.post('/api/avatars', upload.single('avatar'), async(req, res) => {
-  const {path: tempUpload, originalname} = req.files;
-  const resultUpload = path.join(avatarDir, originalname)
-  await fs.rename(tempUpload, resultUpload);
-  const avatar = path.join('avatars', originalname);
-  const newContact = {
-    id: nanoid(),
-    ...req.body,
-    avatar,
-  }
-  avatars.push(newContact);
-  res.status(201).json(newContact);
-})
+app.use(express.static('public'))
 
 app.use('/api/contacts', contactsRouter)
 app.use('/api/auth', authRouter)
 
 app.use((req, res) => {
+  
   res.status(404).json({ message: 'Not found' })
 })
 
 app.use((err, req, res, next) => {
+  
   const {status = 500, message = "Server error"} = err; 
   res.status(status).json({ message });
 })
--- controllers/auth.js
+++ controllers/auth.js
@@ -1,10 +1,15 @@
 const bcryptjs = require("bcryptjs");
 const jwt = require("jsonwebtoken");
 const { User } = require("../models/user");
+const gravatar = require("gravatar");
+const path = require("path");
+const fs = require("fs/promises");
 
 const { HttpError, ctrlWrapper } = require("../helpers");
 
 const { SECRET_KEY } = process.env;
+
+const avatarsDir = path.join(__dirname, "../", "public", "avatars");
 
 const register = async (req, res) => {
   const { email, password } = req.body;
@@ -12,8 +17,11 @@ const register = async (req, res) => {
   if (user) {
     throw HttpError(409, "Email in use");
   }
+
   const hashPassword = await bcryptjs.hash(password, 10);
-  const newUser = await User.create({ ...req.body, password: hashPassword });
+  const avatarURL = gravatar.url(email);
+
+  const newUser = await User.create({ ...req.body, password: hashPassword, avatarURL});
   res.status(201).json({
     email: newUser.email,
     subscription: newUser.subscription,
@@ -51,10 +59,24 @@ res.json({
 })
 }
 
+const updateAvatar = async (req, res) => {
+const {_id} = req.user;
+const {path: tempUpload, originalname} = req.file;
+const filename = `${_id}_${originalname}`;
+const resultUpload = path.join(avatarsDir, filename);
+await fs.rename(tempUpload, resultUpload);
+const avatarURL = path.join('avatars', filename);
+await User.findByIdAndUpdate(_id, {avatarURL});
+res.json({
+  avatarURL,
+})
+}
+
 
 module.exports = {
   register: ctrlWrapper(register),
   login: ctrlWrapper(login),
   getCurrent: ctrlWrapper(getCurrent),
   logout: ctrlWrapper(logout),
+  updateAvatar: ctrlWrapper(updateAvatar),
 };
